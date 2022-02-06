a6cbd2a326de3b4fd183ad8ecd1b91fdf58acf5c
Rafi Septian Hadi <rafiseptian90@gmail.com>
2022-02-06 20:25:17 +0700

    Update ParentCompletnessTest by adding some error testing

--- tests/Unit/ParentCompletnessTest.php
+++ tests/Unit/ParentCompletnessTest.php
@@ -1,38 +1,68 @@
 <?php
 
-use App\Http\Resources\ParentCompletnessResource;
 use App\Models\ParentCompletness;
-use Database\Seeders\ParentCompletnessSeeder;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Artisan;
+use App\Models\User;
+use App\Models\Role;
 
-uses(\Tests\TestCase::class, RefreshDatabase::class);
+uses(\Tests\TestCase::class);
+
+beforeEach(function(){
+    Artisan::call('migrate:fresh --seed');
+
+    $this->superAdminUserToken = User::findOrFail(1)->createToken('secret token', Role::SUPER_ADMIN_PERMISSIONS)->plainTextToken;
+    $this->operatorUserToken = User::findOrFail(2)->createToken('secret token', Role::OPERATOR_PERMISSIONS)->plainTextToken;
+});
+
+afterEach(function(){
+    Artisan::call('migrate:fresh');
+});
 
 it('can fetch all parent completness', function () {
-    $this->seed(ParentCompletnessSeeder::class);
-
-    $this->getJson('api/v1/parent-completness')
+    $this->getJson('api/v1/parent-completness', 
+            // Request Header
+            ['Authorization' => 'Bearer ' . $this->superAdminUserToken]
+         )
          ->assertStatus(200)
-         ->assertExactJson([
-             'code_status' => 200,
-             'msg_status' => 'Parent Completnesses has been loaded',
-             'data' => ParentCompletnessResource::collection(ParentCompletness::latest()->get())
-         ]);
+         ->assertJsonStructure(['code_status', 'msg_status', 'data']);
+});
+
+it('cannot fetch all parent completness cause they dont have that permission', function () {
+    $this->getJson('api/v1/parent-completness', 
+            // Request Header
+            ['Authorization' => 'Bearer ' . $this->operatorUserToken]
+         )
+         ->assertStatus(403);
 });
 
 it('can fetch parent completness', function(){
     $parentCompletness = ParentCompletness::create(['type' => 'Blabla Type', 'score' => 5]);
 
-    $this->getJson('api/v1/parent-completness/' . $parentCompletness->id)
+    $this->getJson('api/v1/parent-completness/' . $parentCompletness->id, 
+            // Request Header
+            ['Authorization' => 'Bearer ' . $this->superAdminUserToken]
+         )
          ->assertStatus(200)
-         ->assertExactJson([
-             'code_status' => 200,
-             'msg_status' => 'Parent Completness has been loaded',
-             'data' => ParentCompletnessResource::make($parentCompletness->load('students'))
-         ]);
+         ->assertJsonStructure(['code_status', 'msg_status', 'data']);
+});
+
+it('cannot fetch parent completness cause they dont have that permission', function(){
+    $parentCompletness = ParentCompletness::create(['type' => 'Blabla Type', 'score' => 5]);
+
+    $this->getJson('api/v1/parent-completness/' . $parentCompletness->id, 
+            // Request Header
+            ['Authorization' => 'Bearer ' . $this->operatorUserToken]
+         )
+         ->assertStatus(403);
 });
 
 it('can create a new parent completness', function(){
-    $this->postJson('api/v1/parent-completness', ['type' => 'Blabla type', 'score' => 5])
+    $this->postJson('api/v1/parent-completness', 
+            // Request Body
+            ['type' => 'Blabla type', 'score' => 5],
+            // Request Header
+            ['Authorization' => 'Bearer ' . $this->superAdminUserToken]
+         )
          ->assertStatus(200)
          ->assertExactJson([
              'code_status' => 200,
@@ -40,10 +70,35 @@ it('can create a new parent completness', function(){
          ]);
 });
 
+it('cannot create a new parent completness cause they dont have that permission', function(){
+    $this->postJson('api/v1/parent-completness', 
+            // Request Body
+            ['type' => 'Blabla type', 'score' => 5],
+            // Request Header
+            ['Authorization' => 'Bearer ' . $this->operatorUserToken]
+         )
+         ->assertStatus(403);
+});
+
+it('cannot create a new parent completness cause there an issue in request body', function(){
+    $this->postJson('api/v1/parent-completness', 
+            // Request Body
+            ['type' => 'Blabla type'],
+            // Request Header
+            ['Authorization' => 'Bearer ' . $this->superAdminUserToken]
+         )
+         ->assertStatus(422);
+});
+
 it('can update a parent completness', function(){
     $parentCompletness = ParentCompletness::create(['type' => 'Blabla Type', 'score' => 5]);
 
-    $this->putJson('api/v1/parent-completness/' . $parentCompletness->id, ['type' => 'Blabla updated type', 'score' => 5])
+    $this->putJson('api/v1/parent-completness/' . $parentCompletness->id,
+            // Request Body 
+            ['type' => 'Blabla updated type', 'score' => 5],
+            // Request Header
+            ['Authorization' => 'Bearer ' . $this->superAdminUserToken]
+        )
         ->assertStatus(200)
         ->assertExactJson([
             'code_status' => 200,
@@ -52,13 +107,54 @@ it('can update a parent completness', function(){
 
 });
 
+it('cannot update a parent completness cause they dont have that permission', function(){
+    $parentCompletness = ParentCompletness::create(['type' => 'Blabla Type', 'score' => 5]);
+
+    $this->putJson('api/v1/parent-completness/' . $parentCompletness->id,
+            // Request Body 
+            ['type' => 'Blabla updated type', 'score' => 5],
+            // Request Header
+            ['Authorization' => 'Bearer ' . $this->operatorUserToken]
+        )
+        ->assertStatus(403);
+});
+
+it('cannot update a parent completness cause there an issue in request body', function(){
+    $parentCompletness = ParentCompletness::create(['type' => 'Blabla Type', 'score' => 5]);
+
+    $this->putJson('api/v1/parent-completness/' . $parentCompletness->id,
+            // Request Body 
+            ['score' => 5],
+            // Request Header
+            ['Authorization' => 'Bearer ' . $this->superAdminUserToken]
+        )
+        ->assertStatus(422);
+});
+
 it('can delete parent completness', function(){
     $parentCompletness = ParentCompletness::create(['type' => 'Blabla Type', 'score' => 5]);
 
-    $this->deleteJson('api/v1/parent-completness/' . $parentCompletness->id)
+    $this->deleteJson('api/v1/parent-completness/' . $parentCompletness->id, 
+            // Request Body
+            [],
+            // Request Header
+            ['Authorization' => 'Bearer ' . $this->superAdminUserToken]
+         )
          ->assertStatus(200)
          ->assertExactJson([
              'code_status' => 200,
              'msg_status' => 'Parent Completness has been deleted'
          ]);
+});
+
+it('cannot delete parent completness cause they dont have that permission', function(){
+    $parentCompletness = ParentCompletness::create(['type' => 'Blabla Type', 'score' => 5]);
+
+    $this->deleteJson('api/v1/parent-completness/' . $parentCompletness->id, 
+            // Request Body
+            [],
+            // Request Header
+            ['Authorization' => 'Bearer ' . $this->operatorUserToken]
+         )
+         ->assertStatus(403);
 });
