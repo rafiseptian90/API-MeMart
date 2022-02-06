c2793dde820282cbe0ecd7e3f7611ff1a49d48ee
Rafi Septian Hadi <rafiseptian90@gmail.com>
2022-02-06 14:18:06 +0700

    Update on Parent Income test by adding some error test

--- tests/Unit/ParentIncomeTest.php
+++ tests/Unit/ParentIncomeTest.php
@@ -1,20 +1,30 @@
 <?php
 
-use App\Http\Resources\ParentIncomeResource;
 use App\Models\ParentIncome;
 use Database\Seeders\ParentIncomeSeeder;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Artisan;
+use App\Models\User;
+use App\Models\Role;
 
-uses (\Tests\TestCase::class, RefreshDatabase::class);
+uses (\Tests\TestCase::class);
 
 beforeEach(function(){
+    Artisan::call('migrate:fresh --seed');
+
+    $this->superAdminUserToken = User::findOrFail(1)->createToken('secret token', Role::SUPER_ADMIN_PERMISSIONS)->plainTextToken;
+    $this->operatorUserToken = User::findOrFail(2)->createToken('secret token', Role::OPERATOR_PERMISSIONS)->plainTextToken;
+
     $this->parentIncome = ParentIncome::create(['amount' => 'More than Rp 5.000.000', 'score' => 1]);
 });
 
-it('can fetch all parent income', function(){
-    $this->seed(ParentIncomeSeeder::class);
+afterEach(function(){
+    Artisan::call('migrate:fresh');
+});
 
-    $this->getJson('api/v1/parent-income')
+it('can fetch all parent income', function(){
+    $this->getJson('api/v1/parent-income', [
+            'Authorization' => 'Bearer ' . $this->superAdminUserToken
+         ])
          ->assertStatus(200)
          ->assertJsonStructure([
              'code_status',
@@ -23,8 +33,17 @@ it('can fetch all parent income', function(){
         ]);
 });
 
+it('cannot fetch all parent income cause they dont have permission to do that', function(){
+    $this->getJson('api/v1/parent-income', [
+            'Authorization' => 'Bearer ' . $this->operatorUserToken
+        ])
+        ->assertStatus(403);
+});
+
 it('can fetch parent income', function(){
-    $this->getJson('api/v1/parent-income/' . $this->parentIncome->id)
+    $this->getJson('api/v1/parent-income/' . $this->parentIncome->id, [
+            'Authorization' => 'Bearer ' . $this->superAdminUserToken
+        ])
          ->assertStatus(200)
          ->assertJsonStructure([
              'code_status',
@@ -33,8 +52,20 @@ it('can fetch parent income', function(){
          ]);
 });
 
+it('cannot fetch parent income cause they dont have permission to do that', function(){
+    $this->getJson('api/v1/parent-income/' . $this->parentIncome->id, [
+            'Authorization' => 'Bearer ' . $this->operatorUserToken
+        ])
+         ->assertStatus(403);
+});
+
 it('can create a new parent income', function(){
-    $this->postJson('api/v1/parent-income', ['amount' => 'More than Rp 7.000.000', 'score' => 1])
+    $this->postJson('api/v1/parent-income', 
+            // Request Body
+            ['amount' => 'More than Rp 7.000.000', 'score' => 1], 
+            // Request Header
+            ['Authorization' => 'Bearer ' . $this->superAdminUserToken]
+         )
          ->assertStatus(200)
          ->assertExactJson([
              'code_status' => 200,
@@ -42,8 +73,33 @@ it('can create a new parent income', function(){
          ]);
 });
 
+it('cannot create a new parent income cause they dont have permission to do that', function(){
+    $this->postJson('api/v1/parent-income', 
+            // Request Body
+            ['amount' => 'More than Rp 7.000.000', 'score' => 1], 
+            // Request Header
+            ['Authorization' => 'Bearer ' . $this->operatorUserToken]
+         )
+         ->assertStatus(403);
+});
+
+it('cannot create a new parent income cause there an issue in a request body', function(){
+    $this->postJson('api/v1/parent-income', 
+            // Request Body
+            ['score' => 1], 
+            // Request Header
+            ['Authorization' => 'Bearer ' . $this->superAdminUserToken]
+         )
+         ->assertStatus(422);
+});
+
 it('can update parent income', function(){
-    $this->putJson('api/v1/parent-income/' . $this->parentIncome->id, ['amount' => 'Less than Rp 500.000', 'score' => 10])
+    $this->putJson('api/v1/parent-income/' . $this->parentIncome->id, 
+            // Request Body
+            ['amount' => 'Less than Rp 500.000', 'score' => 10],
+            // Request Header
+            ['Authorization' => 'Bearer ' . $this->superAdminUserToken]
+         )
          ->assertStatus(200)
          ->assertExactJson([
              'code_status' => 200,
@@ -51,11 +107,46 @@ it('can update parent income', function(){
          ]);
 });
 
+it('cannot update parent income cause they dont have permission to do that', function(){
+    $this->putJson('api/v1/parent-income/' . $this->parentIncome->id, 
+            // Request Body
+            ['amount' => 'Less than Rp 500.000', 'score' => 10],
+            // Request Header
+            ['Authorization' => 'Bearer ' . $this->operatorUserToken]
+         )
+         ->assertStatus(403);
+});
+
+it('cannot update parent income cause there an issue in a request body', function(){
+    $this->putJson('api/v1/parent-income/' . $this->parentIncome->id, 
+            // Request Body
+            ['amount' => 'Less than Rp 500.000'],
+            // Request Header
+            ['Authorization' => 'Bearer ' . $this->superAdminUserToken]
+         )
+         ->assertStatus(422);
+});
+
 it('can delete parent income', function(){
-    $this->deleteJson('api/v1/parent-income/' . $this->parentIncome->id)
+    $this->deleteJson('api/v1/parent-income/' . $this->parentIncome->id, 
+            // Request Body
+            [],
+            // Request Header
+            ['Authorization' => 'Bearer ' . $this->superAdminUserToken]
+         )
          ->assertStatus(200)
          ->assertExactJson([
              'code_status' => 200,
              'msg_status' => 'Parent Income has been deleted'
          ]);
+});
+
+it('cannot delete parent income cause they dont have permission to do that', function(){
+    $this->deleteJson('api/v1/parent-income/' . $this->parentIncome->id, 
+            // Request Body
+            [],
+            // Request Header
+            ['Authorization' => 'Bearer ' . $this->operatorUserToken]
+         )
+         ->assertStatus(403);
 });
